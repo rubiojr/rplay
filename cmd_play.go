7f2f888a31063032cd3831b6bcc35421a3e4d457
Sergio Rubio <rubiojr@frameos.org>
2020-11-03 18:57:28 +0100

    play: implement random
    Play a random track when no ID argument given.

--- cmd_play.go
+++ cmd_play.go
@@ -5,7 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
+	"math/rand"
+	"time"
 
 	"github.com/blugelabs/bluge"
 	"github.com/rubiojr/rapi"
@@ -23,12 +24,43 @@ func init() {
 	appCommands = append(appCommands, cmd)
 }
 
+func randomize() string {
+	var found string
+
+	query := bluge.NewWildcardQuery("*").SetField("_id")
+	request := bluge.NewAllMatches(query)
+
+	c, _ := blugeReader().Count()
+	rand.Seed(time.Now().UnixNano())
+	r := rand.Intn(int(c))
+
+	documentMatchIterator, err := blugeReader().Search(context.Background(), request)
+	counter := int(0)
+	match, err := documentMatchIterator.Next()
+	for err == nil && match != nil && counter < r {
+		err = match.VisitStoredFields(func(field string, value []byte) bool {
+			if field == "_id" {
+				found = string(value)
+			}
+			return true
+		})
+
+		counter += 1
+		match, err = documentMatchIterator.Next()
+	}
+
+	return found
+}
+
 func playSong(c *cli.Context) error {
 	repo, err := rapi.OpenRepository(globalOptions)
 	if err != nil {
 		return err
 	}
 	id := c.Args().Get(0)
+	if id == "" {
+		id = randomize()
+	}
 
 	reader, err := bluge.OpenReader(blugeConf)
 	if err != nil {
@@ -55,8 +87,7 @@ func playSong(c *cli.Context) error {
 	}
 
 	if match == nil {
-		println("Error: no MP3 file found with that ID.")
-		os.Exit(1)
+		return fmt.Errorf("no MP3 file found with ID %s", id)
 	}
 
 	meta := map[string]string{}
