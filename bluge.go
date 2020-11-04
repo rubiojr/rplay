1c8f3999decb561f411e1529285b616549168432
Sergio Rubio <rubiojr@frameos.org>
2020-11-04 17:07:44 +0100

    Fix: don't 💥 when indexing for the second time

--- bluge.go
+++ bluge.go
@@ -7,7 +7,7 @@ var bWriter *bluge.Writer
 
 func blugeReader() *bluge.Reader {
 	var err error
-	if bWriter == nil {
+	if bReader == nil {
 		bReader, err = bluge.OpenReader(blugeConf)
 		if err != nil {
 			panic(err)
