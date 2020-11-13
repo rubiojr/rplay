f97db9837430ae4970fcdbb8cc32eb854e639bb4
Sergio Rubio <rubiojr@frameos.org>
2020-11-13 13:56:57 +0100

    Bump version to 0.3.0

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	app := &cli.App{
 		Name:     "rplay",
 		Commands: []*cli.Command{},
-		Version:  "v0.2.3",
+		Version:  "v0.3.0",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:        "repo",
